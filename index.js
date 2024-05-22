ec95d6ff68c4bd08f392d4437ea4f968669e5a50
SconeForScale <scone.mail.sh@gmail.com>
2024-05-22 01:05:55 -0700

    feat: functional hapi server

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-const Hapi = require('@hapi/hapi')
-//TODO the whole hapi server
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+const Inert = require('@hapi/inert');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: process.env.PORT || 3000,
+        host: 'localhost'
+    });
+
+    await server.register(Inert);
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: Path.join(__dirname, 'dist'),
+                index: true
+            }
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
